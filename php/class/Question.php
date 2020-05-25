20e852a4e9b31657e7e7b6fd08bb1d12b376a8e0
Magdalena Słyk <madzia.slyk@gmail.com>
2020-05-25 20:16:57 +0200

    Dodanie klasy Question

---
+++ php/class/Question.php
@@ -0,0 +1,76 @@
+<?php
+//include "DBConnection.php";
+
+class Question
+{
+    protected $db;
+    private $_id;
+    private $_id_category;
+    private $_question;
+    private $_answerA;
+    private $_answerB;
+    private $_answerC;
+    private $_correct_answer;
+
+    public function setID($id) {
+        $this->_id = $id;
+    }
+    public function setId_category($id_category) {
+        $this->_id_category= $id_category;
+    }
+    public function setQuestion($question) {
+        $this->_question= $question;
+    }
+    public function setAnswerA($answerA) {
+        $this->_answerA = $answerA;
+    }
+    public function setAnswerB($answerB) {
+        $this->_answerB = $answerB;
+    }
+    public function setAnswerC($answerC) {
+        $this->_answerC = $answerC;
+    }
+    public function setCorrect_answer($correct_answer) {
+        $this->_correct_answer = $correct_answer;
+    }
+
+    public function __construct() {
+        $this->db = new DBConnection();
+        $this->db = $this->db->returnConnection();
+    }
+
+    // Assent question method
+    public function updateStatusWhenQuestionAssent(){
+        $query = 'UPDATE qanda SET assent = "1" WHERE id ="'.$this->_id .'"';
+        $result = $this->db->query($query) or die($this->db->error);
+        return true;
+    }
+
+
+    public function getQuestionInfo(){
+        $query = "SELECT * FROM qanda WHERE id = '".$this->_id."'";
+        $result = $this->db->query($query) or die($this->db->error);
+        $count_row = $result->num_rows;
+        if ($count_row == 1) {
+            $animal_data = $result->fetch_array(MYSQLI_ASSOC);
+            return $animal_data;
+        }else{
+            return false;
+        }
+    }
+
+    // Add Question Method
+    public function addQuestion()
+    {
+        $query = 'INSERT INTO qanda SET 
+            id_category="' . $this->_id_category . '",
+            question="' . $this->_question . '",
+            answer_a="' . $this->_answerA . '",
+            answer_b="' . $this->_answerB . '",
+            answer_c="' . $this->_answerC . '",   
+            correct_answer="' . $this->_correct_answer . '"';
+        $result = $this->db->query($query) or die($this->db->error);
+        return true;
+    }
+}
+?>
